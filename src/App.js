0b0f9865cb16b80a616681ae44d470bbd1cf9395
miguelt6 <miguel.t6@gmail.com>
2020-01-12 11:00:22 +0000

    Create-react-app, Jsx, components, Props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Person from "./Person/Person";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My First React App</h1>
+      <Person name="Miguel" age="36">
+        {" "}
+        And I love reading!{" "}
+      </Person>
+      <Person name="Brian" age="47" />
+      <Person name="Laika" age="8" />
     </div>
+    // React.createElement(
+    //   "div",
+    //   null,
+    //   React.createElement("h1", { className: "App" }, "Hello React World")
+    // )
   );
-}
+  //);
+};
 
 export default App;
